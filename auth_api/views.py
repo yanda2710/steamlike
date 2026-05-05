a5a77e340d381e893f508b8b75b16ca7de92fb2c
Yanda <yandaexpcaz@gmail.com>
2026-05-05 21:22:22 +0200

    sem 5 ej 1 maileroo HA FUNCIONADO!!!

--- auth_api/views.py
+++ auth_api/views.py
@@ -1,15 +1,18 @@
-import json
+import json, os, requests, logging
 
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_GET, require_POST
 
 # Importamos el modelo de usuario
 from django.contrib.auth import authenticate, login, get_user_model
 User = get_user_model()
 
 from library.views import error_response
+
+MAILEROO_URL = "https://smtp.maileroo.com/api/v2/emails" # Para enviar correos
+logger = logging.getLogger(__name__)
 
 # POST /api/auth/register/
     # Register a new user
@@ -145,4 +148,46 @@ def check_login(request):
     return JsonResponse({
         "id": request.user.id, # No sé si lo llamará correctamente
         "username": request.user.username
+    }, status=200)
+
+
+# url api/email/send/
+
+@csrf_exempt
+@require_POST
+def send_email(request):
+    data = json.loads(request.body)
+    to = data["to"]
+    subject = data["subject"]
+    text = data["text"]
+
+    headers = {
+        "X-API-Key": os.getenv("MAILEROO_TOKEN", ""),
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "from": {"address": os.getenv("MAILEROO_FROM_ADDRESS", "")},
+        "to": [{"address": to}],
+        "subject": subject,
+        "plain": text
+    }
+
+    try:
+        r = requests.post(MAILEROO_URL, headers=headers, json=payload, timeout=10)
+
+    except requests.RequestException:
+        return JsonResponse({
+            "error": "external_service_unavailable",
+        }, status=503)
+
+    if r.status_code >= 400:
+        logger.error('send_email: external_service_error to="%s" status=%s body="%s"', to, r.status_code, r.text[:200])
+        return JsonResponse({
+            "message": "external_service_error",
+        }, status=502)
+    
+    logger.info('send_email: ok to="%s" status=%s', to, r.status_code, r.text[:200])
+    return JsonResponse({
+        "ok": True
     }, status=200)
--- steamlike_backend/urls.py
+++ steamlike_backend/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from library.views import health
 from library.views import games, game_detailed, search_catalog, resolve_games
 
-from auth_api.views import register, login_view, check_login
+from auth_api.views import register, login_view, check_login, send_email
 
 
 urlpatterns = [
@@ -16,6 +16,7 @@ urlpatterns = [
     path("api/auth/register/", register),
     path("api/auth/login/", login_view),
     path("api/users/me/", check_login),
+    path("api/email/send/", send_email),
     
     # Library API
     path("api/library/entries/", games),
